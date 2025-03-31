4a2e14266719949429bb6a0b6c028f7a9f5de28f
Rio <6583302+rioyin@user.noreply.gitee.com>
2025-03-31 16:14:18 +0800

    新增 UIScreen 扩展

---
+++ Sources/RioSwiftUIKit/Extension/UIScreenExtension.swift
@@ -0,0 +1,5 @@
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+    static let screenSize = UIScreen.main.bounds.size
+}
