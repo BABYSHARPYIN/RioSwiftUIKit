61317d343e0ae8bde205a9d472f557cf808ba781
Rio <6583302+rioyin@user.noreply.gitee.com>
2025-03-31 16:15:01 +0800

    commit

--- Sources/RioSwiftUIKit/Extension/UIScreenExtension.swift
+++ Sources/RioSwiftUIKit/Extension/UIScreenExtension.swift
@@ -1,3 +1,11 @@
+//
+//  UIScreenExtension.swift
+//  RioSwiftUIKit
+//
+//  Created by Rio on 2025/3/31.
+//
+import SwiftUI
+
 extension UIScreen {
     static let screenWidth = UIScreen.main.bounds.width
     static let screenHeight = UIScreen.main.bounds.height
